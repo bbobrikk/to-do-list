03b58cd4241ff01e29ebcd3e0b929e469cb1885d
PC <aaa249148@gmail.com>
2025-11-11 14:41:57 +0300

    create table

---
+++ application/models/task_model.py
@@ -0,0 +1,20 @@
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import CheckConstraint
+from application.core.connection import Base
+
+class TaskORM(Base):
+
+    __tablename__ = "Tasks"
+
+    task_id : Mapped[int] = mapped_column(primary_key=True)
+    title : Mapped[str] = mapped_column(unique=True)
+    description : Mapped[str]
+    status : Mapped[str]
+    deadline : Mapped[date]
+    created_at : Mapped[date]
+
+    __table_args__ = (
+        CheckConstraint("status in ('done', 'completing')", "CHK_status_valid"),
+        CheckConstraint("deadline >= created_at", "CHK_date_valid")
+    )
