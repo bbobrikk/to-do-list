3af522223dcfed10fae17effc4d3a804081d9e39
PC <aaa249148@gmail.com>
2025-11-11 15:33:46 +0300

    add utils

---
+++ application/utils/dependency.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from application.core.connection import session
+
+async def get_session():
+    async with session.begin() as sess:
+        yield sess
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
---
+++ application/utils/setup.py
@@ -0,0 +1,6 @@
+from application.core.connection import Base, engine
+
+async def setup_db():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
