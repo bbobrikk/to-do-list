5eea7be69782838de56db707179053947de5cfa1
PC <aaa249148@gmail.com>
2025-11-11 17:43:56 +0300

    created apies and main

---
+++ application/main.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+from application.utils.setup import setup_db
+from fastapi import FastAPI
+from uvicorn import run
+from application.apies.task_apies import router
+
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+
+    await setup_db()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    run("main:app")
